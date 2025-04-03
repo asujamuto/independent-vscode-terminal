e826621a71d35ff2eeccbe0fe605fb6d63325a97
asujamuto <sokolowskipiotr297@protonmail.com>
2025-04-04 00:21:29 +0200

    cleaned up version

---
+++ src/terminal/terminal.ts
@@ -0,0 +1,95 @@
+import { getTerminalWebviewContent } from './terminalWebView';
+import * as vscode from 'vscode';
+import { execSync } from 'child_process';
+import * as path from 'path';
+
+export const terminal = (currentPanel: vscode.WebviewPanel | undefined, context: vscode.ExtensionContext) => {
+
+        const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor?.viewColumn : undefined;
+        if(currentPanel)
+        {
+                currentPanel.reveal(columnToShowIn);
+        } 
+        else 
+        {
+            currentPanel = vscode.window.createWebviewPanel(
+            'pretty-command-line',
+            'Pretty Terminal',
+             columnToShowIn || vscode.ViewColumn.One,
+             {
+                enableScripts: true
+             },
+             );
+        }
+        
+        let icon = vscode.Uri.file(
+            path.join(context.extensionPath, 'src/icons/terminal_icon.png')
+        );
+        currentPanel.iconPath = icon;
+        
+        const iconUri = currentPanel.webview.asWebviewUri(icon);
+        
+        const updateWebview = () => {
+            
+            if(currentPanel)
+            {
+                const fontColor = 'white';
+                currentPanel.webview.html = getTerminalWebviewContent(iconUri.toString(), fontColor);
+            }
+        };
+        currentPanel.webview.onDidReceiveMessage(
+            message => {
+                switch (message.command) {
+                    case 'terminal-command':
+                        try {
+                        
+                        if(message.text === "clear")
+                        {
+                            return;	
+                        }
+                        if (message.text.startsWith('cd ')) {
+                            const newPath = message.text.slice(3).trim();
+                            process.chdir(newPath);  
+                            return currentPanel?.webview.postMessage({
+                                command: 'output-data',
+                                newDirectory: process.cwd(),
+                                output: JSON.stringify([`${process.cwd()}`])
+                            });
+                        }
+
+                        const output = execSync(message.text, {encoding: 'utf-8'});
+                        const splitted = output.split("\n").filter(line => line !== '');
+                        
+                        currentPanel?.webview.postMessage({ 
+                            command: 'output-data',
+                            output: JSON.stringify(splitted)  
+                        });
+                        
+                        
+                            return;
+                        }
+                        catch(e: unknown)
+                        {
+                            if(e instanceof Error)
+                            {
+                                let errorMesage = e.message.split("\n");
+                                currentPanel?.webview.postMessage({ 
+                                    command: 'output-data',
+                                    output: JSON.stringify(errorMesage)  });
+                                return;
+                            }
+                        }
+                    break;
+                }
+            }
+        );
+
+        updateWebview();
+
+        const interval = setInterval(updateWebview, 1000);
+
+       currentPanel.onDidDispose(() => { currentPanel = undefined; clearInterval(interval); },
+                    null, 
+                    context.subscriptions
+        ); 
+    };
---
+++ src/terminal/terminalWebView.ts
@@ -0,0 +1,160 @@
+import { Webview } from "vscode";
+import * as path from 'path';
+
+import * as vscode from 'vscode';
+import { execSync } from 'child_process';
+
+export function getTerminalWebviewContent(icon: string, fontColor: string)
+{ 
+ 	return `<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Linux Terminal</title>
+     <style>
+ 		@import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+         body {
+             background-color: #1e1e1e;
+ 			font-family: "Work Sans", sans-serif;
+   			font-optical-sizing: auto;
+   			font-weight: <weight>;
+   			font-style: normal;
+			         }
+         .terminal {
+			 z-index:2;
+			 border-radius: 5px;
+			 margin: 0;
+ 		    padding: 15px;
+ 		    border-radius: 5px;
+  			background-color: rgba(0,0,0, 0.4);
+			color: 'white';
+			filter: blue(8px);
+         }
+         .prompt {
+             color: ${fontColor};
+         }
+         input {
+             background: none;
+             border: none;
+             color: ${fontColor};
+             width: auto;
+             outline: none;
+			 z-index:2;
+
+			
+         }
+ 		input:focus {
+             outline: none;
+ 		}
+ 		.output-text {
+ 			color: pin;
+ 			margin: 0;
+ 			padding: 0;
+ 			margin-bottom: 10px;
+ 		}
+ 		.output-container {
+ 			margin-bottom: 20px;
+ 		}
+
+ }
+
+ 	</style>
+ </head>
+ <body>
+ <script>
+     const vscode = acquireVsCodeApi(); 
+     document.addEventListener("DOMContentLoaded", function () {
+          const terminalInput = document.querySelector("#terminal-input-container");
+          terminalInput.querySelector("#terminal-input").focus()
+          const inputField = document.querySelector("input");
+          inputField.addEventListener("keypress", handleEnter);
+      }); 
+ 	 window.addEventListener('message', event => {
+             const message = event.data; 
+             const terminal = document.querySelector(".terminal");
+             const terminalInput = document.querySelector("#terminal-input-container");
+ 			let currentDir;
+             switch (message.command) {
+                 case 'output-data':
+			
+ 					let outputParsed = JSON.parse(message.output);
+ 					let outputContainer = document.createElement("div");
+ 					outputContainer.className = "output-container";
+			
+		
+ 					outputParsed.forEach(i => {
+                 		let outputParagraph = document.createElement("p");
+                 		outputParagraph.innerHTML = "<span style='color: ${fontColor}'>" + i + "</span>";
+ 						outputParagraph.className = "output-text"
+                 		outputContainer.appendChild(outputParagraph); 
+             		});
+ 					terminal.appendChild(outputContainer);
+ 					terminal.appendChild(terminalInput); 
+ 					terminalInput.querySelector("#terminal-input").focus();
+ 					if (message.newDirectory) 
+ 					{
+ 						currentDir = message.newDirectory;
+             			const p = terminal.querySelectorAll(".prompt")
+ 						p.forEach((item) => {
+ 							item.innerHTML =  "user@linux: " + currentDir + "<br/>╰┈➤" ;
+ 						})
+ 					}
+ 				}
+     });
+     function handleEnter(event) {
+         if (event.key === "Enter") {
+             event.preventDefault();
+             const terminal = document.querySelector(".terminal");
+             const terminalInput = document.querySelector("#terminal-input-container");
+             const input = event.target;
+             const command = input.value.trim();                    
+             if(command.split(" ")[0] == "vim" || command.split(" ")[0] == "nano")
+             {
+                const newLine = document.createElement("p");
+                const notSupportedMessage = document.createElement("p");
+                newLine.innerHTML = "<p style='color: ${fontColor}'><span class='prompt'>user@linux: ~<br/>╰┈➤</span>" + command + "</p>";              
+                notSupportedMessage.innerHTML =  "<p style='color: red'>" + "This text editor is not supported yet" + "</p>";              
+                terminal.appendChild(newLine);  
+                terminal.appendChild(notSupportedMessage);  
+                terminal.appendChild(terminalInput); 
+                terminalInput.querySelector("#terminal-input").focus()
+                input.value = "";
+             }
+             else if (command !== "") {
+                 vscode.postMessage({
+                     command: "terminal-command",
+                     text: command
+                 });
+                 const newLine = document.createElement("p");
+                 newLine.innerHTML = "<p style='color: ${fontColor}'><span class='prompt'>user@linux: ~<br/>╰┈➤</span>" + command + "</p>";              
+                 terminal.appendChild(newLine);  
+                 terminal.appendChild(terminalInput); 
+                 terminalInput.querySelector("#terminal-input").focus()
+                 input.value = "";
+                 if(command === "clear")
+                 {
+                    terminal.textContent = "";
+                    terminal.appendChild(terminalInput);
+                    terminalInput.querySelector("#terminal-input").focus()
+                 }
+                 
+             }          
+         }
+     }
+ </script>
+     <div class="terminal">
+ 		<h1 style="display: flex; justify-content: center; align-items: center; gap: 20px">
+                Hello From <p style="font-weight: 500 px; color: #B71CEE; ">Pretty</p> Terminal <img src=${icon} width="64 px"/>
+        </h1>
+         <form onsubmit="return false;">
+             <div id="terminal-input-container">
+                 <span class="prompt" >user@linux: ~<br/>╰┈➤</span> 
+                 <input type="text" id="terminal-input" placeholder="Type a command..." 
+                         autofocus onkeypress="handleEnter(event)">
+             </div>
+         </form>
+     </div>
+ </body>
+ </html>`;
+}
